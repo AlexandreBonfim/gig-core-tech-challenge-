1a690468f9e5555e0a1e3f23f8ee702be67ada89
Alexandre <alebonfinela@gmail.com>
2021-07-21 20:22:22 +0200

    base client

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Chat from './components/chat';
+import Join from './components/join';
+
+const App = () => (
+    <Router>
+        <Route path='/' exact component={Join} />
+        <Route path='/chat'  component={Chat} />
+    </Router>
+        
+);
+
+
+export default App;
---
+++ client/src/decs.d.ts
@@ -0,0 +1,4 @@
+// Does not support typescript as these not supply @types. Rack!
+
+declare module "react-scroll-to-bottom"
+declare module "react-emoji"
